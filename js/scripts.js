c1220adb748dc33f976d5b13e9537232e6357c5f
norihess <Norihess19@gmail.com>
2021-08-18 17:11:00 -0500

    pokemon 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
 let pokemonList ='';
 console.log(typeof pokemonList);
 let pokemon1 =[
-    name: 'ninetales',
+    name: 'Ninetales',
     height: 1.1,
     types: ['fire']
 ];
 let pokemon2=[
-    name: 'houndoom',
+    name: 'Houndoom',
     height: 1.4,
     types: ['fire', 'dark']
+];
+let pokemon3=[
+    name: 'Zekrom',
+    height: 2.9,
+    types: ['dragon', 'electric']
 ];
 
 
