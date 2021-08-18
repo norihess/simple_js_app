6802ed37024ce18bfe496add1bf451a6541b7c31
norihess <Norihess19@gmail.com>
2021-08-18 17:09:20 -0500

    pokemon2 array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 let pokemonList ='';
 console.log(typeof pokemonList);
-
-let pokemon1 ={
-    name: ninetales,
+let pokemon1 =[
+    name: 'ninetales',
     height: 1.1,
     types: ['fire']
-};
+];
+let pokemon2=[
+    name: 'houndoom',
+    height: 1.4,
+    types: ['fire', 'dark']
+];
+
+
