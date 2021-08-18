ad07b1cde2213e52f009a35ff833d7f48b43c1d3
norihess <Norihess19@gmail.com>
2021-08-18 17:15:21 -0500

    updated array and added multiple objects inside with different keys.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,23 @@
-let pokemonList ='';
-console.log(typeof pokemonList);
-let pokemon1 =[
+let pokemonList =[
+{
     name: 'Ninetales',
     height: 1.1,
     types: ['fire']
-];
-let pokemon2=[
+    },
+{
     name: 'Houndoom',
     height: 1.4,
     types: ['fire', 'dark']
-];
-let pokemon3=[
+    },
+{
     name: 'Zekrom',
     height: 2.9,
     types: ['dragon', 'electric']
-];
-let pokemon4=[
+    },
+{
     name: 'Reshiram',
     height: 3.2,
     types: ['dragon', 'fire']
+    }
+
 ];
-
-
